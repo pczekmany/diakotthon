5731d33e488c84c092f496709386ae80d17e2503
Peter <Peter@Peter-TOSH.t.hu>
2014-05-03 20:25:01 +0200

    jobbsáv- hírek menü - leonardo menü -

--- hirek.php
+++ hirek.php
@@ -1,7 +1,8 @@
 <?php
-$result = mysql_query("SELECT cim, tartalom FROM ".$_SESSION[adatbazis_etag]."_szoveg WHERE archiv = '0' LIMIT 5");
+$result = mysql_query("SELECT cim, tartalom, hir FROM ".$_SESSION[adatbazis_etag]."_szoveg WHERE archiv = '0' and hir = '1' LIMIT 5");
 while ($next_element = mysql_fetch_array($result)){
-    
-    $tartalom .= '<h2>'.$next_element[cim].'</h2>'
-            . '<div>'.$next_element[tartalom].'</div>';
+  
+    $tartalom .= '<div id="hirekbox"><h2>'.$next_element[cim].'</h2>'
+            . '<div>'.$next_element[tartalom].'</div></div>';
+ 
 }
